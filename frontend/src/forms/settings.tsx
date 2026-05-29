c48138a776dea1661ae921c84ab53318aea5e4a6
Alessandro Buonocore <60114301+alessandro-ooo@users.noreply.github.com>
2026-05-29 21:56:53 +0200

    chores: remove csl

--- frontend/src/forms/settings.tsx
+++ frontend/src/forms/settings.tsx
@@ -20,7 +20,6 @@ const SettingsForm = ({ data }: SettingsProps) => {
 
   const onSubmit: SubmitHandler<FormInputs> = () => {
     const selectedPort = getValues("ports");
-    console.log(selectedPort);
 
     CloseConnection();
     ConnectToCOM(selectedPort.toString());
