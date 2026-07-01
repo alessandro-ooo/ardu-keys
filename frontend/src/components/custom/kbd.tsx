65f518d95a37926f8625b8c8327120dff5c6cf1c
Alessandro Buonocore <60114301+alessandro-ooo@users.noreply.github.com>
2026-07-01 20:01:55 +0200

    feat: custom keycap

--- frontend/src/components/custom/kbd.tsx
+++ frontend/src/components/custom/kbd.tsx
@@ -1,17 +1,25 @@
-import { Button } from "../ui/button";
-import { Kbd } from "../ui/kbd";
+import { cn } from "@/lib/utils";
 
 type KBDProps = {
   kbdName: string;
+  digital: string;
 };
 
-const KBD = ({ kbdName }: KBDProps) => {
+const KBD = ({ kbdName, digital }: KBDProps) => {
   return (
-    <Button variant="outline">
-      <Kbd className="bg-inherit">
-        <p className="capitalize">{kbdName}</p>
-      </Kbd>
-    </Button>
+    <button
+      className={cn(
+        "h-15 w-15 rounded-md bg-linear-to-tl from-zinc-800 to-zinc-700 flex pt-1 pl-1 pb-2 pr-2 text-xs",
+        // "active:h-13 active:w-13" <- "pressed" effect, TODO: gonna handle it with the board
+      )}
+    >
+      <div className="bg-linear-to-r from-zinc-800 to-zinc-600 h-full w-full rounded-md pt-2 flex justify-center shadow-[inset_0_-3px_0_0_rgba(255,255,255,0.05),inset_0_1px_0_0_rgba(0,0,0,0.4)]">
+        <div className="flex flex-col">
+          <p className="font-semibold text-[13px]">{digital}</p>
+          <p className="capitalize text-[10px]">{kbdName}</p>
+        </div>
+      </div>
+    </button>
   );
 };
 
