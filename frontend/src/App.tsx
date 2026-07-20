902c65ddec481a036028d52d1eb854310e3ff0c3
Alessandro Buonocore <60114301+alessandro-ooo@users.noreply.github.com>
2026-07-20 11:36:30 +0200

    chores: remove poc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,8 @@
 import { Toaster } from "@/components/ui/sonner";
 import { Route } from "wouter";
 import Settings from "./wouter/settings/settings";
-import { Events } from "@wailsio/runtime";
-import { toast } from "sonner";
 
 function App() {
-  Events.On("app:error", (event) => {
-    console.log("FIRED");
-    toast.error(event.data);
-  });
-
   return (
     <div className="min-h-screen w-full bg-zinc-900">
       <Toaster />
